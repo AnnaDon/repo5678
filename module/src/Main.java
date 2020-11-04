718b5d2e50d041b4343f94af85a0eb9805f2915e
AnnaDon <doncilaas@gmail.com>
2020-11-04 13:23:17 +0200

    this branch was rebased to master

--- module/src/Main.java
+++ module/src/Main.java
@@ -2,7 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
     }
+    @Override
+    public String toString() {
+        return "Main{}";
+    }
 public void method1(){};
 public void method2(){};
 
+    public Main() {
+    }
 }
